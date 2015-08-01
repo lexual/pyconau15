edb9726f6cc5692db29b0b844a2d337eb66387c5
lexual <lex@jbadigital.com>
2015-08-01 04:35:37 +0000

    use conda env create in install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,3 @@
 #Download and install Anaconda: http://docs.continuum.io/anaconda/install.html
 
-conda create -n ads python=3.4 numpy scipy scikit-learn pandas xray pillow
-source activate ads
-
-conda install numpy scipy scikit-learn pandas xray pillow
-conda install ipython ipython-notebook matplotlib seaborn
-pip install theano
-pip install keras
-pip install tweepy
-
-git clone https://github.com/danielfrg/word2vec
-cd word2vec
-python setup.py install
-cd ..
-rm -rf word2vec
+conda env create -f environment.yml
